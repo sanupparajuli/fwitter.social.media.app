7f8a4c2945f1858e6d9d9e8dd838415fecc17efc
Sanup Parajuli <sanupparajuli1@gmail.com>
2025-03-09 00:15:52 +0545

    add failure

---
+++ f_twitter_social_media_app/lib/core/error/failure.dart
@@ -0,0 +1,30 @@
+class Failure {
+  final String message;
+  final int? statusCode;
+  Failure({
+    required this.message,
+    this.statusCode,
+  });
+
+  @override
+  String toString() => 'Failure(message: $message, statusCode: $statusCode)';
+}
+
+class LocalDatabaseFailure extends Failure {
+  LocalDatabaseFailure({
+    required super.message,
+  });
+}
+
+class ApiFailure extends Failure {
+  final int? statusCode;
+  @override
+  ApiFailure({
+    this.statusCode,
+    required super.message,
+  });
+}
+
+class SharedPrefsFailure extends Failure {
+  SharedPrefsFailure({required super.message});
+}
